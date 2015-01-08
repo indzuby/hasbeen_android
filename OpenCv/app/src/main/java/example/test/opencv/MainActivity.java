3f664e836ad21f60985668850ae6c7e3367d5ed3
zuby <rlawngus36@gmail.com>
2015-01-08 16:34:46 +0900

    fix(facebook): 페이스북 로그인 오류수정

--- OpenCv/app/src/main/java/example/test/opencv/MainActivity.java
+++ OpenCv/app/src/main/java/example/test/opencv/MainActivity.java
@@ -3,17 +3,31 @@ package example.test.opencv;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.FacebookException;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
 
 import org.opencv.android.OpenCVLoader;
+
+import java.util.Arrays;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    Button btnGallery,btnMap;
+    private String TAG = "MainActivity";
+    Button btnGallery;
+
     static {
         if (!OpenCVLoader.initDebug()) {
             // Handle initialization error
@@ -29,23 +43,52 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnGallery = (Button) findViewById(R.id.btn_gallery);
-        btnMap = (Button) findViewById(R.id.btn_map);
         btnGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(),GalleryActivity.class);
+                Intent intent = new Intent(getBaseContext(), GalleryActivity.class);
                 startActivity(intent);
 
             }
         });
-        btnMap.setOnClickListener(new View.OnClickListener() {
+        LoginButton authButton = (LoginButton) findViewById(R.id.btn_facebookLogin);
+        authButton.setOnErrorListener(new LoginButton.OnErrorListener() {
+
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(),MapActivity.class);
-                startActivity(intent);
+            public void onError(FacebookException error) {
+                Log.i(TAG, "Error " + error.getMessage());
+            }
+        });
+        // set permission list, Don't foeget to add email
+        authButton.setReadPermissions(Arrays.asList("user_likes", "email"));
+        // session state call back event
+        authButton.setSessionStatusCallback(new Session.StatusCallback() {
+
+            @Override
+            public void call(Session session, SessionState state, Exception exception) {
+
+                if (session.isOpened()) {
+                    Log.i(TAG, "Access Token" + session.getAccessToken());
+                    Request.executeMeRequestAsync(session,
+                            new Request.GraphUserCallback() {
+                                @Override
+                                public void onCompleted(GraphUser user, Response response) {
+                                    if (user != null) {
+                                        Log.i(TAG, "User ID " + user.getId());
+                                        Log.i(TAG, "Email " + user.asMap().get("email"));
+                                        Toast.makeText(getBaseContext(), user.asMap().get("email").toString(), Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+                }
 
             }
         });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
     }
 
     @Override
