4fb768944f6af3b8b01c4d63eccedc4aff1ec892
zuby <rlawngus36@gmail.com>
2015-03-20 11:41:44 +0900

    feat(position delete) : 장소 삭제 기능 추가

---
+++ hasBeen/app/src/main/java/co/hasBeen/day/PositionDeleteAsyncTask.java
@@ -0,0 +1,56 @@
+package co.hasBeen.day;
+
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
+
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.HttpDelete;
+
+import co.hasBeen.utils.HasBeenAsyncTask;
+import co.hasBeen.utils.Session;
+
+/**
+ * Created by 주현 on 2015-03-20.
+ */
+public class PositionDeleteAsyncTask  extends HasBeenAsyncTask<Object,Void,Boolean> {
+    //    final static String URL = "https://gist.githubusercontent.com/indzuby/a22f8ae73de9c3e3339c/raw/7f07e297c50961151fe4b8a4839a65d7d176fa91/DayView";
+    final static String URL = Session.DOMAIN+"positions/";
+
+    @Override
+    protected Boolean doInBackground(Object... params) {
+        try {
+            uri = Uri.parse(URL + params[1]);
+            HttpDelete del = new HttpDelete(uri.toString());
+            del.addHeader("User-Agent","Android");
+            del.addHeader("Content-Type", "application/json");
+            del.addHeader("Authorization", "Bearer " + params[0]);
+            response = client.execute(del);
+            StatusLine statusLine = response.getStatusLine();
+            if(statusLine.getStatusCode() == 204)
+                return true;
+            return null;
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Boolean status) {
+
+        Message msg = mHandler.obtainMessage();
+        if(status!=null) {
+            msg.obj = status;
+            msg.what = 0;
+        }else {
+            msg.what = -1;
+        }
+        mHandler.sendMessage(msg);
+    }
+
+    public PositionDeleteAsyncTask(Handler handler) {
+        super(handler);
+    }
+}
